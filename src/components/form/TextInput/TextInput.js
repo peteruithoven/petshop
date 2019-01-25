7872ae6b6f6ba688a7b2dd2082db7dfbbbf9cbeb
Peter Uithoven <peter@peteruithoven.nl>
2019-01-25 17:57:07 +0100

    Use TextField helperText prop

--- src/components/form/TextInput/TextInput.js
+++ src/components/form/TextInput/TextInput.js
@@ -4,12 +4,7 @@ import styled from "styled-components";
 import { units } from "../../../theme.js";
 
 const Container = styled.div`
-  margin: 0 ${units(3)} ${units(1)} ${units(3)}
-`;
-
-const Feedback = styled.div`
-  color: ${props => props.theme.palette.error.main};
-  margin-top: ${units(1)};
+  margin: 0 ${units(3)} ${units(1)} ${units(3)};
 `;
 
 export default function TextInput({ type = "text", form, field, ...props }) {
@@ -22,10 +17,10 @@ export default function TextInput({ type = "text", form, field, ...props }) {
         id={name}
         type={type}
         error={touched && !!error}
+        helperText={touched && error}
         {...field}
         {...props}
       />
-      {touched && error && <Feedback>{error}</Feedback>}
     </Container>
   );
 }
