7c323437ddeb8688d7e53c504d6f2b125a139278
Peter Uithoven <peter@peteruithoven.nl>
2019-06-04 01:04:18 +0200

    Update dependencies

--- src/components/ThemeProviders/ThemeProviders.js
+++ src/components/ThemeProviders/ThemeProviders.js
@@ -1,28 +1,28 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
-import { MuiThemeProvider } from "@material-ui/core/styles";
-import JssProvider from 'react-jss/lib/JssProvider';
-import { create } from 'jss';
-import { createGenerateClassName, jssPreset } from '@material-ui/core/styles';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/styles';
+// import JssProvider from 'react-jss/lib/JssProvider';
+// import { create } from 'jss';
+// import { createGenerateClassName, jssPreset } from '@material-ui/core/styles';
 import theme from "../../theme.js";
 
 // customize material-ui insertion point so that
 // styled-components overrides material-ui styles
-const generateClassName = createGenerateClassName();
-const jss = create({
-  ...jssPreset(),
-  // We define a custom insertion point that JSS will look for injecting the styles in the DOM.
-  insertionPoint: 'jss-insertion-point',
-});
+// const generateClassName = createGenerateClassName();
+// const jss = create({
+//   ...jssPreset(),
+//   // We define a custom insertion point that JSS will look for injecting the styles in the DOM.
+//   insertionPoint: 'jss-insertion-point',
+// });
 
 export default function ThemeProviders({children}) {
   return (
-    <JssProvider jss={jss} generateClassName={generateClassName}>
+    // <JssProvider jss={jss} generateClassName={generateClassName}>
       <MuiThemeProvider theme={theme}>
         <ThemeProvider theme={theme}>
           {children}
         </ThemeProvider>
       </MuiThemeProvider>
-    </JssProvider>
+    // </JssProvider>
   )
 }
