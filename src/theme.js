72ab096a79241475cf769929fab92d24facffe06
Peter Uithoven <peter@peteruithoven.nl>
2019-06-04 14:11:33 +0200

    Theme cleanup

--- src/theme.js
+++ src/theme.js
@@ -1,20 +1,12 @@
-import { createMuiTheme } from "@material-ui/core/styles";
-import orange from '@material-ui/core/colors/orange';
-import red from '@material-ui/core/colors/red';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { red, orange } from '@material-ui/core/colors';
 
 export default createMuiTheme({
-  typography: {
-    useNextVariants: true
-  },
   palette: {
     primary: orange,
     secondary: red,
   },
   shape: {
-    borderRadius: 6
-  }
+    borderRadius: 6,
+  },
 });
-
-export function units (num) {
-  return props => `${props.theme.spacing.unit * num}px`;
-}
